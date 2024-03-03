c6d998def25e096118f6d64c494218f2aa3f018b
NayemHassan <nayem15-12498@diu.edu.bd>
2024-03-03 18:14:42 +0600

    new commit

--- resources/views/frontend/home_page/team/team.blade.php
+++ resources/views/frontend/home_page/team/team.blade.php
@@ -11,27 +11,56 @@
                      </div>
                  </div>
                  <div class="row team-active drInfo">     
-
-                     
-                      
                    
-             </div>
+                 </div>
          </section>
          
          <script>
-         $(document).ready(function() {
+    $(document).ready(function() {
        $.ajax({
         url:"https://dr.sobrokom.store/api/doctor/dr_info/1",
         type:'GET',
         success:function(res){
-            console.log(res.);
+          //  console.log(res.drInfo);
             let drInfo ="";
             $.each(res.drInfo, function( key, value){
-                 console.log(value.name);
-                // console.log(value.long_caption);
-              
+               // console.log(value.dr_image);
+                
+              drInfo +=    `<div class="col-xl-4 ">
+                         <div class="single-team mb-30" >
+                             <div class="team-thumb">
+                                 <div class="brd">
+                                      <img height="300px" width="300" src="https://dr.sobrokom.store/uploads/dr_image/${value.dr_image}" alt="img">
+                                 </div>
+                                 
+                                 <div class="dropdown">
+                                   <a class="xbtn"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                     +
+                                   </a>
+                                   <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                     <div class="team-social mt-15">
+                                     <ul>
+                                         <li><a href="#"><i class="fab fa-facebook-f"></i></a></li> 
+                                         <li> <a href="#"><i class="fab fa-twitter"></i></a></li>   
+                                         <li><a href="#"><i class="fab fa-instagram"></i></a></li>   
+                                     </ul>       
+                                 </div>
+                                   </div>
+                                 </div>
+                                 
+                                 
+                             </div>
+                             <div class="team-info">
+                                 <h4>${value.name}</h4>
+                                 <span>Internist, General Practitoner</span>
+                                 <p>Working Since 2004</p>
+                             </div>
+                         </div>
+                     </div>
+                     </div>
+            `
             });
-            $('.drInfo').html(drInfo);
+           $('.drInfo').html(drInfo);
         }
        });
     })
